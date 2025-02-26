a2304df15676a1ce3a639378e24097a30080884a
Evan <41494790+evaan@users.noreply.github.com>
2025-02-26 16:09:09 -0330

    add to imports

--- machines/sotetseg/configuration.nix
+++ machines/sotetseg/configuration.nix
@@ -12,6 +12,7 @@
     ./../../services/games/tmodloader.nix
     ./../../services/misc/beszel/hub.nix
     ./../../services/misc/beszel/agent-sotetseg.nix
+    ./../../services/misc/monitoring/grafana.nix
     ./../../services/misc/monitoring/prometheus.nix
     ./../../services/misc/monitoring/node-exporter.nix
     ./../../services/misc/monitoring/dcgm-exporter.nix
