4a405d4f2ab98a77aa840c35feadd5d8c15585e3
Evan <41494790+evaan@users.noreply.github.com>
2025-02-26 23:04:08 -0330

    asdfkl

--- services/misc/monitoring/prometheus.nix
+++ services/misc/monitoring/prometheus.nix
@@ -4,25 +4,25 @@
     enable = true;
     globalConfig.scrape_interval = "1m";
     scrapeConfigs = [
-    {
-      job_name = "sotetseg";
-      static_configs = [{
-        targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" "localhost:9400" "localhost:8087" ];
-      }];
-    }
-    {
-      job_name = "kephri";
-      static_configs = [{
-        targets = [ "192.168.2.8:${toString config.services.prometheus.exporters.node.port}" "192.168.2.8:8087" ];
-      }];
-    }
+      {
+        job_name = "sotetseg";
+        static_configs = [{
+          targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" "localhost:9400" "localhost:8087" ];
+        }];
+      }
+      {
+        job_name = "kephri";
+        static_configs = [{
+          targets = [ "192.168.2.8:${toString config.services.prometheus.exporters.node.port}" "192.168.2.8:8087" ];
+        }];
+      }
+      {
+        job_name = "runescape";
+        static_configs = [{
+          targets = [ "localhost:8340" "localhost:8341" "localhost:8342" ];
+        }];
+      }
     ];
-    {
-      job_name = "runescape";
-      static_configs = [{
-        targets = [ "localhost:8340" "localhost:8341" "localhost:8342" ];
-      }];
-    };
   };
 
   networking.firewall.allowedTCPPorts = [ 9090 ];
