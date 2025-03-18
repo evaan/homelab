95bcd780c76bfed62e1f267adf801c4b8add6959
Evan <41494790+evaan@users.noreply.github.com>
2025-03-18 12:18:07 -0230

    Replace nginx with glance

---
+++ services/misc/glance.nix
@@ -0,0 +1,11 @@
+{ lib, ... }: {
+  virtualisation.oci-containers.containers."glance" = {
+    image = "docker.io/glanceapp/glance";
+    autoStart = true;
+    ports = [ "84:8080" ];
+    volumes = [
+      "glance-config:/app/config"
+      "/etc/localtime:/etc/localtime:ro"
+    ];
+  };
+}
