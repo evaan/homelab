c09413d548b4df00b4ca533ca7cef5bd5cf228f0
Evan <41494790+evaan@users.noreply.github.com>
2025-03-21 14:40:24 -0230

    Run plex on baremetal

--- services/media/plex.nix
+++ services/media/plex.nix
@@ -1,20 +1,6 @@
-{ lib, config, ... }: {
-  virtualisation.oci-containers.containers."plex" = {
-    image = "docker.io/linuxserver/plex:latest";
-    autoStart = true;
-    extraOptions = [
-      "--runtime=nvidia"
-      "--gpus=all"
-    ];
-    ports = [ "32400:32400" "1900:1900/udp" "5353:5353/udp" "8324:8324" "32410:32410/udp" "32412-32414:32412-32414/udp" "32469:32469" ];
-    environment = {
-      PUID = "1000";
-      PGID = "1000";
-    };
-    volumes = [
-      "plex-config:/config"
-      "/mnt/storage:/media"
-      "/etc/localtime:/etc/localtime:ro"
-    ];
-  };
+{ ... }: {
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  }
 }
