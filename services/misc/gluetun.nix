3d3769263b50494daa5e139aa6dee6f6f8af4635
Evan <41494790+evaan@users.noreply.github.com>
2025-03-30 00:16:53 -0230

    rdt vpn

--- services/misc/gluetun.nix
+++ services/misc/gluetun.nix
@@ -2,7 +2,7 @@
   virtualisation.oci-containers.containers."gluetun" = {
     image = "docker.io/qmcgaw/gluetun:latest";
     autoStart = true;
-    ports = [ "8080:8080" "8081:8081" ];
+    ports = [ "8080:8080" "8081:8081" "6500:6500" ];
     extraOptions = [ "--cap-add=NET_ADMIN" "--device=/dev/net/tun:/dev/net/tun" ];
     environmentFiles = [
       config.sops.secrets.gluetun-env.path
