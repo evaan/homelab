8c0af2449fedb9b9202fcd1bc936e6cc108728ab
Evan <41494790+evaan@users.noreply.github.com>
2025-03-30 00:02:03 -0230

    Add rdt-client

---
+++ services/media/rdt-client.nix
@@ -0,0 +1,18 @@
+{ lib, config, ... }: {
+  virtualisation.oci-containers.containers."rdtclient" = {
+    image = "docker.io/rogerfar/rdtclient:latest";
+    autoStart = true;
+    ports = [
+      "6500:6500"
+    ];
+    volumes = [
+      "rdtclient_data:/data/db"
+      "rdtclient_downloads:/data/downloads"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "1000";
+      TZ = "Europe/London";
+    };
+  };
+}
