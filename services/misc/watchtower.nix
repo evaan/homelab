04b2b836b84130c1640767ba1f6985ea1354ae58
Evan <41494790+evaan@users.noreply.github.com>
2025-04-16 03:25:42 -0230

    i need tests

--- services/misc/watchtower.nix
+++ services/misc/watchtower.nix
@@ -7,7 +7,7 @@
     ];
     environment = {
       "WATCHTOWER_RUN_ONCE" = "true";
-    }
+    };
     volumes = [
       "/var/run/docker.sock:/var/run/docker.sock"
       "/etc/localtime:/etc/localtime:ro"
