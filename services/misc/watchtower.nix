c6c4cff40a7ce123bcd5650184f87411211f9a57
Evan <41494790+evaan@users.noreply.github.com>
2025-04-16 03:17:44 -0230

    watchtower typo

--- services/misc/watchtower.nix
+++ services/misc/watchtower.nix
@@ -1,6 +1,6 @@
 { lib, ... }: {
   virtualisation.oci-containers.containers."watchtower" = {
-    image = "docker.io/containerrr/watchtower:latest";
+    image = "docker.io/containrrr/watchtower:latest";
     autoStart = true;
     volumes = [
       "/var/run/docker.sock:/var/run/docker.sock"
