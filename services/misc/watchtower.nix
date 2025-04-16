3f03d9a0086f5e936de8b3c88a9e321046935ebc
Evan <41494790+evaan@users.noreply.github.com>
2025-04-16 03:25:25 -0230

    idk

--- services/misc/watchtower.nix
+++ services/misc/watchtower.nix
@@ -2,8 +2,15 @@
   virtualisation.oci-containers.containers."watchtower" = {
     image = "docker.io/containrrr/watchtower:latest";
     autoStart = true;
+    environmentFiles = [
+      config.sops.secrets.watchtower-env.path
+    ];
+    environment = {
+      "WATCHTOWER_RUN_ONCE" = "true";
+    }
     volumes = [
       "/var/run/docker.sock:/var/run/docker.sock"
+      "/etc/localtime:/etc/localtime:ro"
     ];
   };
 }
